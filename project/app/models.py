dfd6012f7d747904451acf8f751f905cfba9549e
Adhiraj Singh <adhirajsingh3103.com>
2024-01-20 19:47:53 +0530

    third commit

--- project/app/models.py
+++ project/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 # Create your models here.
+#second commit
 class Student(models.Model):
     Firstname = models.CharField(max_length=100)
     Lastname = models.CharField(max_length=100)
